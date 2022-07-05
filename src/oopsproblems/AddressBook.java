af372f8c9fbcd450e69ba00af2750240411e0e2f
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-05 15:28:07 +0530

    Added: Ability to Create Contact

--- src/oopsproblems/AddressBook.java
+++ src/oopsproblems/AddressBook.java
@@ -1,10 +1,13 @@
 package oopsproblems;
+import java.util.Scanner;
 
 public class AddressBook {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Address Book Program");
+		PersonDetails pd =new PersonDetails ();
+		pd.show();
 	}
 
 }
