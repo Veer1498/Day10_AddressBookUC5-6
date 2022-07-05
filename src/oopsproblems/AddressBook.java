363796bd22f77a9931f150df547c3fba6b4c9acb
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-05 17:11:55 +0530

    Added: Ability to add Contact

--- src/oopsproblems/AddressBook.java
+++ src/oopsproblems/AddressBook.java
@@ -2,12 +2,44 @@ package oopsproblems;
 import java.util.Scanner;
 
 public class AddressBook {
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Address Book Program");
-		PersonDetails pd =new PersonDetails ();
-		pd.show();
-	}
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Enter firstName");
+		String firstName =sc.next();
+		
+		System.out.println("Enter LastName");
+		String lastName =sc.next();
+
+		System.out.println("Enter Address");
+		String address =sc.next();
+		
+		System.out.println("Enter City");
+		String city =sc.next();
+		
+		System.out.println("Enter State");
+		String state =sc.next();
+
+		System.out.println("Enter Zip");
+		int zipNo =sc.nextInt();
+
+		System.out.println("Enter phoneNumber");
+		long phoneNo =sc.nextLong();
+		
+		PersonDetails pd[] =new PersonDetails [5];
+
+		for(int i=0;i<pd.length;i++) {
+			pd[i] = new PersonDetails(firstName,lastName, address, city, state, zipNo, phoneNo);
+		}
+		
+		for(int i=0;i<pd.length;i++) {
+		pd[i].show();
+		System.out.println("");
+		}
+	}
+	
 }
--- src/oopsproblems/PersonDetails.java
+++ src/oopsproblems/PersonDetails.java
@@ -2,50 +2,142 @@ package oopsproblems;
 import java.util.Scanner;
 
 public class PersonDetails {
-	String firstName;
-	String lastName;
-	String address;
-	 String city;
-	String state;
-	int zipNo;
-	long phoneNo;
-	static Scanner sc = new Scanner(System.in);
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zipNo;
+	private long phoneNo;
+	//static Scanner sc = new Scanner(System.in);
 
-	public PersonDetails() {
+	public PersonDetails(String firstName,String lastName, String address, String city, String state, int zipNo, long phoneNo ) {
 
-		System.out.println("Enter firstName");
-		firstName =sc.next();
+	//	System.out.println("Enter firstName");
+	//	firstName =sc.next();
+		this.firstName =firstName;
 		
-		System.out.println("Enter LastName");
-		lastName =sc.next();
+	//	System.out.println("Enter LastName");
+		//lastName =sc.next();
+		this.lastName =lastName;
+
+	//	System.out.println("Enter Address");
+		//address =sc.next();
+		this.address =address;
 		
-		System.out.println("Enter Address");
-		address =sc.next();
+	//	System.out.println("Enter City");
+		//city =sc.next();
+		this.city =city;
 		
-		System.out.println("Enter City");
-		city =sc.next();
-		
-		System.out.println("Enter State");
-		state =sc.next();
-		
-		System.out.println("Enter Zip");
-		zipNo =sc.nextInt();
-		
-		System.out.println("Enter phoneNumber");
-		phoneNo =sc.nextLong();
+	//	System.out.println("Enter State");
+		//state =sc.next();
+		this.state =state;
+
+	//	System.out.println("Enter Zip");
+		//zipNo =sc.nextInt();
+		this.zipNo =zipNo;
+
+	//	System.out.println("Enter phoneNumber");
+		//phoneNo =sc.nextLong();
+		this.phoneNo =phoneNo;
+	}
+	
+	
+	
+	public String getFirstName() {
+		return firstName;
 	}
 
-	
-	
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+
+	public String getState() {
+		return state;
+	}
+
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+
+	public int getZipNo() {
+		return zipNo;
+	}
+
+
+
+	public void setZipNo(int zipNo) {
+		this.zipNo = zipNo;
+	}
+
+
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+
+
 	public void show() {
 		// TODO Auto-generated method stub
 		System.out.println( "Person Details" );
-		System.out.println("FirstName = "+ firstName);
-		System.out.println("Address = "+ address);
-		System.out.println("city = "+ city);
-		System.out.println("state = "+ state);
-		System.out.println("zip = " +zipNo);
-		System.out.println("phoneNumber =" + phoneNo);
+		System.out.println("FirstName = "+ this.firstName);
+		System.out.println("LastName = "+ this.lastName);
+		System.out.println("Address = "+ this.address);
+		System.out.println("city = "+ this.city);
+		System.out.println("state = "+ this.state);
+		System.out.println("zip = " +this.zipNo);
+		System.out.println("phoneNumber = " + this.phoneNo);
 	}
 
 }
