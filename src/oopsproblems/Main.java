a1f3549dd01ea7f04c8d5fdb501e044e76961536
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-07 16:25:44 +0530

    Added : Ability to Add Multiple AddressBooks to System

---
+++ src/oopsproblems/Main.java
@@ -0,0 +1,15 @@
+package oopsproblems;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Address Book Program");
+		
+		Books bookList = new Books();
+		System.out.println("");
+				bookList.NewBook();
+				//addressList.editContact();
+				//addressList.deleteContact();	
+	}
+
+}
